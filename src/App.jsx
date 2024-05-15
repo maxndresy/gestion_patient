7ddcbeb6e41380d6bf0da0355f661427b89cacb9
maxndresy <mandresy.rakotoarivelo@gmail.com>
2024-05-15 06:38:17 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "bulma/css/bulma.min.css";
+const App = () => {
+  return (
+    <div>
+      <p>Bonjour</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import "bulma/css/bulma.min.css";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
